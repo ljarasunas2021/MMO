7a0644a65a45bd4bba8dae7028e29e22640c5135
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2019-09-12 18:59:17 -0700

    refractor: Refractor code to make it flow more nicely

---
+++ Assets/Scripts/ChangeCurrentState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCurrentState : StateMachineBehaviour
+{
+    public int value;
+    public bool onEnter;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (onEnter) animator.SetInteger(Parameters.currentState, value);
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (!onEnter) animator.SetInteger(Parameters.currentState, value);
+    }
+}
