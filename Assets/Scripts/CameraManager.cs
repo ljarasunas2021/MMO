01e0d116473384d21b982919895f9531eeb20702
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2019-09-16 18:10:17 -0700

    feat: Add basic multiplayer

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using Cinemachine;
+
+public class CameraManager : NetworkBehaviour
+{
+    public override void OnStartLocalPlayer()
+    {
+        if (!isLocalPlayer) return;
+        CinemachineFreeLook freeLook = Camera.main.transform.parent.GetChild(1).GetComponent<CinemachineFreeLook>();
+        freeLook.Follow = transform;
+        freeLook.LookAt = transform;
+    }
+}
