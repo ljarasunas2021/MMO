1e5f3c268ed6740efd2696ea765508bdf7aa4a1d
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2019-09-24 15:19:22 -0700

    feat: Make basic server auth w client prediciton

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,15 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using Mirror;
 using Cinemachine;
+using Mirror;
+using UnityEngine;
 
-public class CameraManager : NetworkBehaviour
-{
-    public override void OnStartLocalPlayer()
-    {
+public class CameraManager : NetworkBehaviour {
+    public override void OnStartLocalPlayer () {
+        base.OnStartLocalPlayer ();
         if (!isLocalPlayer) return;
-        CinemachineFreeLook freeLook = Camera.main.transform.parent.GetChild(1).GetComponent<CinemachineFreeLook>();
+        CinemachineFreeLook freeLook = Camera.main.transform.parent.GetChild (1).GetComponent<CinemachineFreeLook> ();
         freeLook.Follow = transform;
         freeLook.LookAt = transform;
     }
