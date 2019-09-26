e5cdd7637cfe1cb28305a672c45215caae40b271
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2019-09-26 13:01:17 -0700

    feat: Add movement that syncs

--- Assets/Scripts/ChangeCurrentState.cs
+++ Assets/Scripts/ChangeCurrentState.cs
@@ -3,26 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 ///<summary> Change current state at animation start / end
-public class ChangeCurrentState : StateMachineBehaviour {
-    [Header ("Enter")]
+public class ChangeCurrentState : StateMachineBehaviour
+{
+    [Header("Enter")]
     // should the variable be changed on state enter
     public bool onEnter;
     // value that the current state should change to when entering
     public int enterValue;
 
-    [Header ("Exit")]
+    [Header("Exit")]
     // should the variable be changed on state exit
     public bool onExit;
     // value that the current state should change to when exiting
     public int exitValue;
 
     ///<summary> if on enter will set the integer </summary>
-    public override void OnStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        if (onEnter) animator.SetInteger (Parameters.currentState, enterValue);
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (onEnter) animator.SetInteger(Parameters.currentState, enterValue);
     }
 
     ///<summary> if on exit will set the integer </summary>
-    public override void OnStateExit (Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        if (onExit) animator.SetInteger (Parameters.currentState, exitValue);
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (onExit) animator.SetInteger(Parameters.currentState, exitValue);
+        Debug.Log("FINISH");
     }
 }
