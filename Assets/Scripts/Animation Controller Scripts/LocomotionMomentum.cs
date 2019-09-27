886631a18a879b99b4c23bfcf78fde5ef9b978ce
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2019-09-27 16:52:11 -0700

    refractor: Make code look good

---
+++ Assets/Scripts/Animation Controller Scripts/LocomotionMomentum.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+///<Summary> Add momentum between locomotion states </summary>
+public class LocomotionMomentum : StateMachineBehaviour
+{
+    #region Variables
+    [Header("Values")]
+    // locomotion blend value when playing idle, walking, and running animations
+    public float idleVal, walkVal, runVal;
+    [Header("Speeds")]
+    // speed player moves by when in idle, walking, and running animations
+    public float idleSpeed, walkSpeed, runSpeed;
+    [Header("Smooth Times")]
+    // how fast player accelerates, decelerates
+    public float accelerationSmoothTime, decelerationSmoothTime;
+
+    //velocity of the change in speed
+    private float smoothVelocity;
+    // target speed velocity should reach
+    private float targetSpeed;
+    #endregion
+
+    #region SetSpeed
+    ///<summary> Set speed of player based on locomotion blend value
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateUpdate(animator, stateInfo, layerIndex);
+
+        float locomotionBlendVal = animator.GetFloat(Parameters.locomotionBlend);
+
+        if (locomotionBlendVal < walkVal)
+        {
+            animator.SetFloat(Parameters.currentSpeed, ((walkSpeed - idleSpeed) / (walkVal - idleVal)) * (locomotionBlendVal - idleVal) + idleSpeed);
+        }
+        else
+        {
+            animator.SetFloat(Parameters.currentSpeed, ((runSpeed - walkSpeed) / (runVal - walkVal)) * (locomotionBlendVal - walkVal) + walkSpeed);
+        }
+    }
+    #endregion
+}
