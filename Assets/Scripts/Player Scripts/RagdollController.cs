2abcebda638f13f91e854c7c2b15b56f138526db
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2019-10-03 16:29:36 -0700

    feat: Add inventory

---
+++ Assets/Scripts/Player Scripts/RagdollController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Mirror;
+
+public class RagdollController : NetworkBehaviour
+{
+    public Rigidbody[] bodyParts;
+
+    private Animator animator;
+    private CharacterController characterController;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    [Command]
+    public void CmdBecomeRagdoll()
+    {
+        animator.enabled = false;
+        characterController.detectCollisions = false;
+        foreach (Rigidbody bodyPart in bodyParts)
+        {
+            bodyPart.isKinematic = false;
+        }
+    }
+}
