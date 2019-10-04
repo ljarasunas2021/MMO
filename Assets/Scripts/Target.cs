998f817938d1ffbde92e55d8dedfe4e33254cbc2
slevy2020 <slevy@chadwickschool.org>
2019-10-04 09:15:08 -0700

    feat: add outline when npc is moused over

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -3,8 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour {
+    
+    private Outline outline;
 
+    void Start() {
+        outline = gameObject.GetComponent<Outline>();
+        outline.enabled = false;
+    }
     public void Interact() {
         gameObject.transform.Rotate(0, 90, 0);
+    }
+
+    void OnMouseEnter() {
+        outline.enabled = true;
+    }
+    void OnMouseExit() {
+        outline.enabled = false;
     }
 }
