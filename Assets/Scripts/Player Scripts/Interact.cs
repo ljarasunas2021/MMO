9348dc2dcf673b11ccb706747c2eb24cd2f42ec8
slevy2020 <slevy@chadwickschool.org>
2019-10-04 08:42:33 -0700

    fix: raycast to interact now works properly

--- Assets/Scripts/Player Scripts/Interact.cs
+++ Assets/Scripts/Player Scripts/Interact.cs
@@ -20,7 +20,7 @@ public class Interact : MonoBehaviour {
 
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            Vector3 point = new Vector3(_camera.pixelWidth/2, _camera.pixelWidth/2, 0);
+            Vector3 point = new Vector3(_camera.pixelWidth/2, _camera.pixelHeight/2, 0);
             Ray ray = _camera.ScreenPointToRay(point);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit)) {
