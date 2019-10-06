f5203e74b5f619f553cbfa4141e3fe24d3f38db9
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2019-10-06 13:21:08 -0700

    feat: Allow for switching between cameras

--- Assets/Scripts/Player Scripts/PlayerCameraManager.cs
+++ Assets/Scripts/Player Scripts/PlayerCameraManager.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 ///<summary> Preform all actions related to player and the camera </summary>
 public class PlayerCameraManager : NetworkBehaviour
 {
+    private GameObject head;
+    private CameraModes currentCam;
+
+    private CameraController cameraController;
+    private CinemachineFreeLook cinematicFreeLook, closeUpFreeLook, lockedFreeLook;
+
     #region SetCameraAtStart
     ///<summary> Set the camera to follow you </summary>
     public override void OnStartLocalPlayer()
@@ -13,21 +19,56 @@ public class PlayerCameraManager : NetworkBehaviour
 
         if (!isLocalPlayer) return;
 
-        GameObject head = GetComponent<BodyParts>().head;
+        head = GetComponent<BodyParts>().head;
+        cameraController = Camera.main.GetComponent<CameraController>();
 
-        CameraController cameraController = Camera.main.GetComponent<CameraController>();
-
-        CinemachineFreeLook cinematicFreeLook = cameraController.cinematicCam.GetComponent<CinemachineFreeLook>();
+        cinematicFreeLook = cameraController.cinematicCam.GetComponent<CinemachineFreeLook>();
         cinematicFreeLook.Follow = head.transform;
         cinematicFreeLook.LookAt = head.transform;
 
-        CinemachineFreeLook closeUpFreeLook = cameraController.closeUpCam.GetComponent<CinemachineFreeLook>();
+        closeUpFreeLook = cameraController.closeUpCam.GetComponent<CinemachineFreeLook>();
         closeUpFreeLook.Follow = head.transform;
         closeUpFreeLook.LookAt = head.transform;
 
-        CinemachineFreeLook lockedFreeLook = cameraController.lockedCam.GetComponent<CinemachineFreeLook>();
+        lockedFreeLook = cameraController.lockedCam.GetComponent<CinemachineFreeLook>();
         lockedFreeLook.Follow = head.transform;
         lockedFreeLook.LookAt = head.transform;
+
+        currentCam = CameraModes.cinematic;
     }
     #endregion
+
+    public void ChangeCam(CameraModes mode)
+    {
+        if (mode != currentCam)
+        {
+            if (mode == CameraModes.cinematic)
+            {
+                cinematicFreeLook.Priority = 1;
+                closeUpFreeLook.Priority = 0;
+                lockedFreeLook.Priority = 0;
+            }
+            else if (mode == CameraModes.closeUp)
+            {
+                cinematicFreeLook.Priority = 0;
+                closeUpFreeLook.Priority = 1;
+                lockedFreeLook.Priority = 0;
+            }
+            else if (mode == CameraModes.locked)
+            {
+                cinematicFreeLook.Priority = 0;
+                closeUpFreeLook.Priority = 0;
+                lockedFreeLook.Priority = 1;
+            }
+
+            currentCam = mode;
+        }
+    }
+}
+
+public enum CameraModes
+{
+    cinematic,
+    closeUp,
+    locked
 }
