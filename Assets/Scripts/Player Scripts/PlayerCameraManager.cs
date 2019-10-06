f85ffe3db840471e58bc7f19792d4a322bd5f8ec
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2019-10-06 11:17:24 -0700

    feat: Add fingers to model

--- Assets/Scripts/Player Scripts/PlayerCameraManager.cs
+++ Assets/Scripts/Player Scripts/PlayerCameraManager.cs
@@ -1,33 +1,62 @@
-﻿using Cinemachine;
-using Mirror;
-using UnityEngine;
+﻿using UnityEngine;
 
-///<summary> Preform all actions related to player and the camera </summary>
-public class PlayerCameraManager : NetworkBehaviour
+public class PlayerEquip : MonoBehaviour
 {
-    #region SetCameraAtStart
-    ///<summary> Set the camera to follow you </summary>
-    public override void OnStartLocalPlayer()
+    public int maxGrabDistance;
+
+    private BodyParts bodyParts;
+    private InventoryManager inventoryManager;
+    private InputHandler inputHandler;
+    private GameObject handR;
+    private ItemHolding currentItemHolding;
+
+    private void Start()
     {
-        base.OnStartLocalPlayer();
-
-        if (!isLocalPlayer) return;
-
-        GameObject head = GetComponent<BodyParts>().head;
-
-        CameraController cameraController = Camera.main.GetComponent<CameraController>();
-
-        CinemachineFreeLook cinematicFreeLook = cameraController.cinematicCam.GetComponent<CinemachineFreeLook>();
-        cinematicFreeLook.Follow = head.transform;
-        cinematicFreeLook.LookAt = head.transform;
-
-        CinemachineFreeLook closeUpFreeLook = cameraController.closeUpCam.GetComponent<CinemachineFreeLook>();
-        closeUpFreeLook.Follow = head.transform;
-        closeUpFreeLook.LookAt = head.transform;
-
-        CinemachineFreeLook lockedFreeLook = cameraController.lockedCam.GetComponent<CinemachineFreeLook>();
-        lockedFreeLook.Follow = head.transform;
-        lockedFreeLook.LookAt = head.transform;
+        bodyParts = GetComponent<BodyParts>();
+        inventoryManager = GetComponent<InventoryManager>();
+        inputHandler = GetComponent<InputHandler>();
+        handR = bodyParts.handR;
     }
-    #endregion
+
+    public void Grab()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
+        if (Physics.Raycast(ray, out hit, maxGrabDistance, LayerMaskController.item) && hit.collider.gameObject.GetComponent<Weapon>() != null)
+        {
+            GameObject weapon = hit.collider.gameObject;
+            weapon.GetComponent<Rigidbody>().isKinematic = true;
+            weapon.transform.SetParent(handR.transform);
+            weapon.transform.localPosition = Vector3.zero;
+            weapon.transform.rotation = Quaternion.identity;
+            inventoryManager.AddInventoryItem(weapon, null);
+            inputHandler.ChangeItemHolding(new ItemHolding(weapon, HoldingItemType.ranged));
+            currentItemHolding = new ItemHolding(weapon, HoldingItemType.ranged);
+        }
+    }
+}
+
+public class ItemHolding
+{
+    public GameObject item;
+    public HoldingItemType type;
+    public Weapon weaponScript;
+
+    public ItemHolding(GameObject item, HoldingItemType type)
+    {
+        this.item = item;
+        this.type = type;
+
+        if (type == HoldingItemType.ranged) weaponScript = item.GetComponent<Weapon>();
+        else weaponScript = null;
+    }
+}
+
+public enum HoldingItemType
+{
+    melee,
+    ranged,
+    collectable,
+    none
 }
