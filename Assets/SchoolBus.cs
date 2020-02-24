2defb31f79faa4f95e2e1cf114c6db3b44c7eea1
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2020-02-24 12:21:12 -0800

    feat: Add bus

---
+++ Assets/SchoolBus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class SchoolBus : NetworkBehaviour
+{
+    public float radius, speed;
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        float theta = Random.Range(0, 2 * Mathf.PI);
+        transform.parent.position = new Vector3(Mathf.Cos(theta) * radius, transform.position.y, Mathf.Sin(theta) * radius);
+        Quaternion rot = Quaternion.LookRotation(new Vector3(0, transform.position.y, 0) - transform.parent.position, Vector3.up);
+        transform.parent.rotation = Quaternion.Euler(rot.eulerAngles + Vector3.up * 90);
+    }
+
+    void Update()
+    {
+        transform.parent.Translate(transform.up * speed);
+    }
+}
