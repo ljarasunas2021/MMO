6f476823c6acc85ead24843cde4bb97c4d701b41
Lukas Jarasunas <lukasjarasunas@Lukass-MacBook-Pro-2.local>
2020-09-19 16:38:04 -0700

    feat: Finish up for checkin, organize heiarchy, find bugs to fix for the next checkin

--- Assets/Scripts/Weapon Scripts/Item.cs
+++ Assets/Scripts/Weapon Scripts/Item.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// item for inventory
 public class Item : MonoBehaviour
 {
+    // icon to be displayed in inventory
     public Sprite icon;
 }
