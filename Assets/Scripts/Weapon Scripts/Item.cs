29695ff9c6c696aeb32a1da8c5d167c76954c76c
Lukas Jarasunas <lukasjarasunas@Lukass-MacBook-Pro-2.local>
2020-09-25 15:34:20 -0700

    feat: Create better comments for the weapon scripts folder

--- Assets/Scripts/Weapon Scripts/Item.cs
+++ Assets/Scripts/Weapon Scripts/Item.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// item for inventory
+/// <summary> Item for inventory</summary>
 public class Item : MonoBehaviour
 {
     // icon to be displayed in inventory
