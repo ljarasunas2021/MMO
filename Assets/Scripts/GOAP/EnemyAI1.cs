1adf463f62735ee1b290eb8c137e46391ac23629
Lukas Jarasunas <lukasjarasunas@Lukass-MacBook-Pro-2.local>
2020-09-27 19:34:32 -0700

    feat: Put transform deep child extension on its own file

--- Assets/Scripts/GOAP/EnemyAI1.cs
+++ Assets/Scripts/GOAP/EnemyAI1.cs
@@ -156,26 +156,3 @@ public class EnemyAI1 : GoapAgent
         }
     }
 }
-
-/// <summary> Find a child of a gameobject with a certain name </summary>
-public static class TransformDeepChildExtension
-{
-    /// <summary> Find a child of a gameobject with a certain name </summary>
-    /// <param name="aParent"> the transform of the parent that this function should search children of </param>
-    /// <param name="aName"> the name of the child this function should search for </param>
-    /// <returns> the transform of the child </returns>
-    public static Transform FindDeepChild(this Transform aParent, string aName)
-    {
-        Queue<Transform> queue = new Queue<Transform>();
-        queue.Enqueue(aParent);
-        while (queue.Count > 0)
-        {
-            var c = queue.Dequeue();
-            if (c.name == aName)
-                return c;
-            foreach (Transform t in c)
-                queue.Enqueue(t);
-        }
-        return null;
-    }
-}
