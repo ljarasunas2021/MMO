530dbf26d5230795e2ba3d89d6024badefe23548
Cooper Saye <coopersaye@gmail.com>
2021-01-08 13:52:21 -0800

    Create quest item.

---
+++ Assets/Scripts/UI/Quest System/QuestItem.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class QuestItem : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private TextMeshProUGUI titleText = null;
+    [SerializeField] private TextMeshProUGUI descriptionText = null;
+
+    // Initialize quest item with title and description
+    public void Initialize(string title, string description)
+    {
+        titleText.text = title;
+        descriptionText.text = description;
+    }
+}
