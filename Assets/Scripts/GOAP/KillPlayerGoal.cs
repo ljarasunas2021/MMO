7e7c2dd8422e2b3c229f78fee537b890ad94a718
Cooper Saye <coopersaye@gmail.com>
2021-01-08 13:04:26 -0800

    Initial commit.

---
+++ Assets/Scripts/GOAP/KillPlayerGoal.cs
@@ -0,0 +1,24 @@
+﻿using SwordGC.AI.Goap;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> The kill player goal for the GOAP Agent. To learn about GOAP AI, visit https://gamedevelopment.tutsplus.com/tutorials/goal-oriented-action-planning-for-a-smarter-ai--cms-20793  </summary>
+public class KillPlayerGoal : GoapGoal
+{
+    /// <summary> Constructor for the KillPlayerGoal </summary>
+    /// <param name="key"> unique name of the goal </param>
+    /// <param name="multiplier"> The cost multiplier. Can be used to create a tendency towards a goal </param>
+    /// <returns> the KillPlayerGoal object </returns>
+    public KillPlayerGoal(string key, float multiplier = 1) : base(key, multiplier)
+    {
+
+    }
+
+    /// <summary> Update the multiplier (does nothing now) </summary>
+    /// <param name="data"> the collective dataset for all AI </param>
+    public override void UpdateMultiplier(DataSet data)
+    {
+        //this.multiplier = (data.data[GoapAction.Effects.PLAYER_DEAD + "0"]) ? 100 : 1;
+    }
+}
---
+++ Assets/Scripts/Not My Scripts/SwitchScenes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour
+{
+    public string sceneName;
+    public void SwitchGame()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
