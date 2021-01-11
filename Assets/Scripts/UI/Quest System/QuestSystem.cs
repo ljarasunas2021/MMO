976e19ee2446548c7585cef0718f4c375a477484
Cooper Saye <coopersaye@gmail.com>
2021-01-11 11:28:02 -0800

    Clean up conflicts from merge.

--- Assets/Scripts/UI/Quest System/QuestSystem.cs
+++ Assets/Scripts/UI/Quest System/QuestSystem.cs
@@ -2,75 +2,78 @@
 using Mirror;
 using UnityEngine;
 
-public class QuestSystem : NetworkBehaviour
+namespace MMO.UI.QuestSystem
 {
-    [Header("References")]
-    [SerializeField] private Canvas canvas = null;
-    [SerializeField] private Transform contentTransform = null;
-    [SerializeField] private GameObject questPrefab = null, progressQuestPrefab = null;
-
-    // QuestSystem singleton
-    public static QuestSystem instance;
-
-    // Active property which automatically updates canvas visibility
-    private bool _active = false;
-    public bool Active
+    public class QuestSystem : NetworkBehaviour
     {
-        get { return _active; }
-        set
+        [Header("References")]
+        [SerializeField] private Canvas canvas = null;
+        [SerializeField] private Transform contentTransform = null;
+        [SerializeField] private GameObject questPrefab = null, progressQuestPrefab = null;
+
+        // QuestSystem singleton
+        public static QuestSystem instance;
+
+        // Active property which automatically updates canvas visibility
+        private bool _active = false;
+        public bool Active
         {
-            // Return if value same, otherwise set value
-            if (value == _active) return;
-            _active = value;
+            get { return _active; }
+            set
+            {
+                // Return if value same, otherwise set value
+                if (value == _active) return;
+                _active = value;
 
-            // Set canvas 
-            canvas.enabled = Active;
+                // Set canvas 
+                canvas.enabled = Active;
+            }
         }
-    }
 
-    // Quest items stored by key and reference to GameObject
-    private Dictionary<string, GameObject> questItems = new Dictionary<string, GameObject>();
+        // Quest items stored by key and reference to GameObject
+        private Dictionary<string, GameObject> questItems = new Dictionary<string, GameObject>();
 
-    private void Awake()
-    {
-        // Initialize singleton
-        if (instance != null) Destroy(gameObject);
-        else instance = this;
-    }
+        private void Awake()
+        {
+            // Initialize singleton
+            if (instance != null) Destroy(gameObject);
+            else instance = this;
+        }
 
-    // Creates a quest with given key, title, and description
-    public void CreateQuest(string key, string title, string description, bool progressBar)
-    {
-        // If key already contained in quest items, return
-        if (questItems.ContainsKey(key)) return;
+        // Creates a quest with given key, title, and description
+        public void CreateQuest(string key, string title, string description, bool progressBar)
+        {
+            // If key already contained in quest items, return
+            if (questItems.ContainsKey(key)) return;
 
-        // Instantiate and initialize quest item at content
-        GameObject prefab = progressBar ? progressQuestPrefab : questPrefab;
-        GameObject questItem = Object.Instantiate(prefab, Vector3.zero, Quaternion.identity, contentTransform);
-        questItem.GetComponent<Quest>().Initialize(title, description);
+            // Instantiate and initialize quest item at content
+            GameObject prefab = progressBar ? progressQuestPrefab : questPrefab;
+            GameObject questItem = Object.Instantiate(prefab, Vector3.zero, Quaternion.identity, contentTransform);
+            questItem.GetComponent<Quest>().Initialize(title, description);
 
-        // Add quest item to dictionary
-        questItems.Add(key, questItem);
-    }
+            // Add quest item to dictionary
+            questItems.Add(key, questItem);
+        }
 
-    // Sets quest progress by key
-    public void SetQuestProgress(string key, float val)
-    {
-        // If key not in quest items, return
-        if (!questItems.ContainsKey(key)) return;
+        // Sets quest progress by key
+        public void SetQuestProgress(string key, float val)
+        {
+            // If key not in quest items, return
+            if (!questItems.ContainsKey(key)) return;
 
-        // If ProgressQuest component not null, set progress to val
-        questItems[key].GetComponent<ProgressQuest>()?.SetProgress(val);
-    }
+            // If ProgressQuest component not null, set progress to val
+            questItems[key].GetComponent<ProgressQuest>()?.SetProgress(val);
+        }
 
-    // Removes a quest by key
-    public void ResolveQuest(string key)
-    {
-        // If key not in quest items, return
-        if (!questItems.ContainsKey(key)) return;
+        // Removes a quest by key
+        public void ResolveQuest(string key)
+        {
+            // If key not in quest items, return
+            if (!questItems.ContainsKey(key)) return;
 
-        // Destroy corresponding GameObject and remove from dictionary
-        Destroy(questItems[key]);
-        questItems.Remove(key);
+            // Destroy corresponding GameObject and remove from dictionary
+            Destroy(questItems[key]);
+            questItems.Remove(key);
+        }
     }
 }
